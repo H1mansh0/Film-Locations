f35b344d8e2889db79d947a990cb409fc7eec7a8
Vladyslav Atamaniuk <115092059+H1mansh0@users.noreply.github.com>
2023-02-21 23:27:58 +0200

    Add modules for functions and adds argparse

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import folium
+import argparse
+from geopy.geocoders import Nominatim
+import haversine
 
+
+geolocator = Nominatim(user_agent="my_requests")
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('longitude', type=int, help='Longitude of place')
+parser.add_argument('latitude', type=int, help='Latitude of place')
+parser.add_argument('year', type=str, help='Year of film')
+
+
+args = parser.parse_args()
+
+ln = args.latitude
+lg = args.longitude
+year = args.year
