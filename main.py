735c49cb9fa4178b57e97a4739cd1629919b91a2
Vladyslav Atamaniuk <115092059+H1mansh0@users.noreply.github.com>
2023-02-21 23:25:02 +0200

    Add function distance_calc

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-
+def distance_calc(ln: int, lg: int, places: list[tuple]) -> list[tuple]:
+    """
+    Calculates the nearest recording places
+    to given coordinates
+    """
+    res = []
+    for ele in places:
+        res += [(ele[0], ele[-1], haversine.haversine((ln, lg), (ele[-1][0], ele[-1][0])))]
+    return sorted(res, key = lambda x: x[-1])[:10]
