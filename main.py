0c3752477bbc7aad4405c29361cc71d0a16febfc
Vladyslav Atamaniuk <115092059+H1mansh0@users.noreply.github.com>
2023-02-21 23:15:22 +0200

    Add function with documentations

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-
+def country_getter(lat: float, long: float) -> str:
+    """
+    Calculates address using
+    longitude and latitude
+    """
+    location = geolocator.geocode([lat, long], language='en').address
+    return location.split(', ')[-1]
