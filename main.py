b829dbc45dea6d35928f4aee17159fd3bc1c034d
Vladyslav Atamaniuk <115092059+H1mansh0@users.noreply.github.com>
2023-02-21 17:25:49 +0200

    Create function file_reader(without documentation)

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-
+def file_reader(filename: str) -> list:
+    res = []
+    with open(filename, 'r') as file:
+        data = file.readlines()[14:-1]
+        for line in data:
+            res += [(line[:-2].split(' (')[0] ,line[:-2].split(' (')[1][:4],
+                                [ele for ele in line.replace('\n', '').split('\t')[1:] if ele][0])]
+    return res
